aa92dd87eced7e68039bf697093dd4a5c1849de4
Joe Chop <joe.chop@prominentedge.com>
2017-11-03 13:39:34 -1000

    updated environment variable

--- docker-run.sh
+++ docker-run.sh
@@ -5,4 +5,4 @@ rm -rf /usr/src/kibana/build/kibana-5.5.3-SNAPSHOT-linux-x86_64/src/ui/theme
 ln -s /usr/src/kibana/build/kibana-5.5.3-SNAPSHOT-linux-x86_64/src/ui/themes/$THEME/ /usr/src/kibana/build/kibana-5.5.3-SNAPSHOT-linux-x86_64/src/ui/theme
 
 # Run Kibana
-/usr/src/kibana/build/kibana-5.5.3-SNAPSHOT-linux-x86_64/bin/kibana -e $ELASTICSEARCH_URL --server.host="0.0.0.0"
+/usr/src/kibana/build/kibana-5.5.3-SNAPSHOT-linux-x86_64/bin/kibana -e $ELASTICSEARCH_URI --server.host="0.0.0.0"

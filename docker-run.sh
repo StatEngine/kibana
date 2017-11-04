d644ffe7d3c0b618fe4e2570bebe0de445647197
Joe Chop <joe.chop@prominentedge.com>
2017-11-04 12:57:28 -1000

    added env variables

--- docker-run.sh
+++ docker-run.sh
@@ -5,4 +5,10 @@ rm -rf /usr/src/kibana/build/kibana-5.5.3-SNAPSHOT-linux-x86_64/src/ui/theme
 ln -s /usr/src/kibana/build/kibana-5.5.3-SNAPSHOT-linux-x86_64/src/ui/themes/$THEME/ /usr/src/kibana/build/kibana-5.5.3-SNAPSHOT-linux-x86_64/src/ui/theme
 
 # Run Kibana
-/usr/src/kibana/build/kibana-5.5.3-SNAPSHOT-linux-x86_64/bin/kibana -e $ELASTICSEARCH_URI --server.host="0.0.0.0"
+/usr/src/kibana/build/kibana-5.5.3-SNAPSHOT-linux-x86_64/bin/kibana \
+  -e $ELASTICSEARCH_URI \
+  --server.host="0.0.0.0" \
+  --elasticsearch.username=$KIBANA_ELASTICSEARCH_USERNAME \
+  --elasticsearch.password=$KIBANA_ELASTICSEARCH_PASSWORD \
+  --elasticsearch.requestHeadersWhitelist=authorization,X-Forwarded-User \
+  --readonlyrest_kbn.proxy_auth_passthrough=true
